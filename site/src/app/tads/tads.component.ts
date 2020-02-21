fbd82f3b163ee6bc3c50f8325d0cf9e2de2a04cd
KarenViolim <karen.violim2011@gmail.com>
2020-02-20 22:30:54 -0300

    Início do Desenvolvimento

---
+++ site/src/app/tads/tads.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tads-component',
+  templateUrl: './tads.component.html',
+  styleUrls: ['./tads.component.css']
+})
+export class TadsComponent {
+}
